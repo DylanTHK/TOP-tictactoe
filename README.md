1019fb94b65e7889a524c133c7fca4e13dc95b9b
DylanTHK <dylantanhk@gmail.com>
2022-08-22 21:48:49 +0800

    FINAL EDIT FOR UPLOAD
    - script.js deleted
    - script2.js renamed to script.js
    - scriptv3.js deleted
    - README.md updated

--- README.md
+++ README.md
@@ -3,10 +3,12 @@ Tic Tac Toe Game
 
 Purpose
 - to create a simple game
-- using factory functions and objects
+- using factory functions and modules
 
 Learning
-- 
+- How to break code into modules
+- Managing private variables and methods
+- Exporting and updating public methods
 
 Project inspired by the Odin's Project, by DylanTHK
 https://www.theodinproject.com/lessons/node-path-javascript-tic-tac-toe
--- script.js
+++ script.js
@@ -1,103 +1,137 @@
-// factory function to create players
-const players = (marker) => {
-    const getMarker = () => {
-        return marker;
-    }
-    return {getMarker};
-}
+// fatory function to generate players
+const Player = (marker) => {
+    this.marker = marker;
+    // function to obtain the marker of player
+    const getMarker = () => marker;
+    return {getMarker}
+};
 
-// To manage board (module)
-const gameBoard = (() => {
-    // set starting empty array
-    const boardArray = ["", "", "", "", "", "", "", "", ""];
-    // update Array with marker input 
-    const updateArray = (marker, index) => {
-        boardArray[index] = marker;
-    }
-    // to return value of cell / array
-    const getArray = (index) => {
-        return boardArray[index];
-    }
-    // to restart game (link to restart button)
-    const resetArray = () => {
-        for(let i = 0; i < boardArray.length; i++) {
-            boardArray[i] = "";
-        }
-    }
-    return {boardArray, updateArray, getArray, resetArray};
-})();
-
-// module to update display
-const updateDisplay = (() => {
-    const announcements = document.getElementById("announcement");
-    const cells = document.querySelectorAll(".tttCells");
-    const restartButton = document.querySelector("#restart-button");
-    
-    // adding event listener for each grid cell
-    cells.forEach((cell) => 
-        cell.addEventListener("click", (e) => {
-            let marker = gameController.currentMarker();
-            let index = e.target.getAttribute("cell-index");
-            e.target.textContent = marker;
-            gameBoard.updateArray(marker, index);
-    }))
-
-    // event listener for restarting game
-    restartButton.addEventListener("click", () => {
-        gameBoard.resetArray();
-    });
-    
-    // function to place marker on cell
-    const updateBoard = (marker, cell) => {
-        if(cell.textContent === "") {
-            cell.textContent = marker;
-        } else return;
-    }
-    // function to update the message section
-    const updateMessage = (message) => {
-        announcements.textContent = message;
-    }
-    
-    return {cells, updateBoard, updateMessage};
-})();
-
+// module to control flow of overall game
 const gameController = (() => {
-    const playerX = players("x");
-    const playerO = players("o");
-    
+    // 1. initialise public variables 
+    const playerX = Player("x");
+    const playerO = Player("o");
     let round = 1;
-    let roundOver = false;
-
-    // to get 
-    let currentMarker = () => {
-        return (round % 2 == 0) ? playerO.getMarker() : playerX.getMarker();
-    }
-
-    const playRound = () => {
-        if(round < 9) {
-            round++
+    let gameOver = false;
+    const winConditions = [
+        [0, 1, 2], 
+        [3, 4, 5], 
+        [6, 7, 8], 
+        [0, 3, 6], 
+        [1, 4, 7], 
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [2, 4, 6]
+    ];
+    // function to determine current Marker based on round
+    let getMarker = () => {
+        return (round % 2 === 0) ? playerO.getMarker() : playerX.getMarker();
+    };
+    // to RESET Board and Array
+    const resetRound = (event) => {
+        round = 1;
+        gameOver = false;
+        gameBoard.reset();
+        displayController.updateAnnouncement("Welcome!! X starts First");
+        displayController.updateGameBoard(gameBoard.boardArray);
+    };
+    // ***** function to facilitate game play (1 round) *****//
+    const playRound = (event) => {
+        // 3. update game board (Array)
+        gameBoard.updateBoardArray(event);
+        // 4. update board display 
+        displayController.updateGameBoard(gameBoard.boardArray);
+        winLogic();
+        // change players
+        displayController.updateAnnouncement();
+        round++;
+    };
+    // 5. Obtain array of all current Marker's positions
+    let indexArray = (board) => {
+        let tempArray = [];
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === getMarker()) {
+                tempArray.push(i);
+            }
         }
-    }
-
-    const resetGame = () => {
-        return
-    }
-
-    // Logic for winning
-
-    //
-    return {currentMarker, round, playerO};
+        return tempArray;
+    };
+    // 6. Check indexArray with winConditions Array (updates gameOver status)
+    const winLogic = () => {
+        // option 1 
+        const gameOutCome = winConditions.some(condition => {
+            return condition.every(el => {
+                return indexArray(gameBoard.boardArray).includes(el); //
+            })
+        });
+        // updates status of the game
+        if (gameOutCome) {
+            gameOver = true;
+        }
+    };
+    const getState = () => gameOver;
+    const getRound = () => round;
+    // making variables public
+    return {playerO, playerX, getState, getRound, getMarker, playRound, winLogic, resetRound};
 })();
 
+// module to keep track of game board Array
+const gameBoard = (() => {
+    let boardArray = ["", "", "", "", "", "", "", "", ""];
+    // 3. Update gameArray (1D Array) based on selected cell index with current marker
+    const updateBoardArray = (event) => {
+        // a) obtain index of selected cell
+        let selectedCell = event.target.getAttribute("cell-index");
+        // b) use index to update boardArray with current Marker
+        if (boardArray[selectedCell] === "") {
+            boardArray[selectedCell] = gameController.getMarker();
+        }
+    };
+    // for loop works but forEach does not update boardArray object?
+    const reset = () => {
+        for (let i = 0; i < boardArray.length; i++) {
+          boardArray[i] = "";
+        }
+      };
+    return {boardArray, updateBoardArray, reset}
+})();
 
-
-
-
-// To switch players after each turn (factory function)
-const changeMarker = () => {
-    if (currentMarker === "x") {
-        currentMarker = "o";
-    } else {
-        currentMarker = "x";
-    }
-}
+// module to update all displays
+const displayController = (() => {
+    // to link with event listeners
+    const announcement = document.querySelector("#announcement");
+    const restartButton = document.querySelector("#restart-button");
+    const cells = document.querySelectorAll(".tttCells");
+    // 2a. Player select TTT cell -> link to playRound
+    cells.forEach((cell) => {
+        cell.addEventListener("click", (e) => {
+            if (!gameController.getState() & cell.textContent === "") gameController.playRound(e);
+        })
+    })
+    // 2b. Reset button -> resets round
+    restartButton.addEventListener("click", (e) => {
+        gameController.resetRound();
+    })
+    // 4. Update gameBoard with values from boardArray
+    const updateGameBoard = (board) => {
+        // add current Marker to the targeted cell (event)
+        for (let i = 0; i < 9; i++) {
+            cells[i].textContent = board[i];
+        }
+    };
+    // 7. update Announcement text (Win / Lose / Draw / Continue)
+    const updateAnnouncement = () => {
+        // Draw Condition
+        if (!gameController.getState & gameController.getRound() > 9) {
+            announcement.textContent = "It's a Draw!";
+        } else if (gameController.getState()) { // win condition
+            announcement.textContent = 
+            `Congrats! Player ${gameController.getMarker().toUpperCase()} has Won!`;
+        } else if (gameController.getRound() % 2 === 1) { // normal condition
+            announcement.textContent = `Player ${gameController.playerO.getMarker().toUpperCase()}'s turn`;
+        } else {
+            announcement.textContent = `Player ${gameController.playerX.getMarker().toUpperCase()}'s turn`;
+        }
+    };
+    return {updateAnnouncement, updateGameBoard};
+})();
